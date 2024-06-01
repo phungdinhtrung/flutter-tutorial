fdfaae6f92f7c3b7ead180d3d4a3d2bf9ec4a0b5
Phung Dinh Trung <phungdinhtrung@gfd.com.vn>
2024-06-02 06:56:25 +0700

    Finish app_level4

---
+++ app_level4/lib/quize_brain.dart
@@ -0,0 +1,30 @@
+import 'question.dart';
+
+class QuizeBrain {
+  int _questionNumber = 0;
+
+  final List<Question> _questionBank = [
+    Question(
+        questionText: 'You can lead a cow down stairs but not up stairs.',
+        questionAnswer: false),
+    Question(
+        questionText:
+            'Approximately one quarter of human bones are in the feet.',
+        questionAnswer: true),
+    Question(questionText: 'A slug\'s blood is green.', questionAnswer: true),
+  ];
+
+  String getQuestionText() {
+    return _questionBank[_questionNumber].questionText;
+  }
+
+  bool getQuestionAnswer() {
+    return _questionBank[_questionNumber].questionAnswer;
+  }
+
+  void nextQuestion() {
+    if (_questionNumber < _questionBank.length - 1) {
+      _questionNumber++;
+    }
+  }
+}
