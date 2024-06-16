ff0a66bf270255cf919d64543d7ca66d8f725f63
Phung Dinh Trung <phungdinhtrung@gfd.com.vn>
2024-06-16 23:14:59 +0700

    Doing app_level 7

---
+++ app_level7/lib/price-screen.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'coin-data.dart';
+
+class PriceScreen extends StatefulWidget {
+  const PriceScreen({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<PriceScreen> createState() => _PriceScreenState();
+}
+
+class _PriceScreenState extends State<PriceScreen> {
+  String selectedCurrency = 'USD';
+
+  List<DropdownMenuItem<String>> getDropdownItems() {
+    List<DropdownMenuItem<String>> dropdownItems = [];
+
+    for (String currency in currenciesList) {
+      var newItem = DropdownMenuItem(
+        value: currency,
+        child: Text(currency),
+      );
+      dropdownItems.add(newItem);
+    }
+    return dropdownItems;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.primary,
+          title: Text(
+            widget.title,
+            style: const TextStyle(color: Colors.white),
+          ),
+          centerTitle: true,
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            Container(
+              padding: const EdgeInsets.all(10.0),
+              margin: const EdgeInsets.all(20.0),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15.0),
+                color: Theme.of(context).colorScheme.secondary,
+              ),
+              alignment: Alignment.center,
+              child: const Text(
+                '1 BTC = ? USD',
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 20.0, color: Colors.white),
+              ),
+            ),
+          ],
+        ),
+        bottomNavigationBar: BottomAppBar(
+          color: Theme.of(context).colorScheme.primary,
+          child: Container(
+            height: 50.0,
+            alignment: Alignment.center,
+            child: DropdownButton<String>(
+              items: getDropdownItems(),
+              onChanged: (String? value) {
+                setState(() {
+                  selectedCurrency = value.toString();
+                });
+              },
+              value: selectedCurrency, // Default value
+              icon: const Icon(Icons.arrow_drop_down, color: Colors.white),
+              dropdownColor: Theme.of(context).colorScheme.primary,
+              style: const TextStyle(color: Colors.white),
+            ),
+          ),
+        ));
+  }
+}
